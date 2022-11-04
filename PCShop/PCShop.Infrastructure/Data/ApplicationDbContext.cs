831ffba3758c9392d1f588798bc7907d7f58751c
Lyubomir <lyubomir.rashkov88@gmail.com>
2022-11-04 15:15:15 +0200

    Refactoring

--- PCShop/PCShop.Infrastructure/Data/ApplicationDbContext.cs
+++ PCShop/PCShop.Infrastructure/Data/ApplicationDbContext.cs
@@ -25,101 +25,101 @@ namespace PCShop.Infrastructure.Data
         /// <summary>
         /// Collection of laptops
         /// </summary>
-        public DbSet<Laptop> Laptops { get; set; }
+        public DbSet<Laptop> Laptops { get; set; } = null!;
 
         /// <summary>
         /// Collection of monitors
         /// </summary>
-        public DbSet<Monitor> Monitors { get; set; }
+        public DbSet<Monitor> Monitors { get; set; } = null!;
 
         /// <summary>
         /// Collection of keyboards
         /// </summary>
-        public DbSet<Keyboard> Keyboards { get; set; }
+        public DbSet<Keyboard> Keyboards { get; set; } = null!; 
 
         /// <summary>
         /// Collection of mice
         /// </summary>
-        public DbSet<Mouse> Mice { get; set; }
+        public DbSet<Mouse> Mice { get; set; } = null!;
 
         /// <summary>
         /// Collection of headphones
         /// </summary>
-        public DbSet<Headphone> Headphones { get; set; }
+        public DbSet<Headphone> Headphones { get; set; } = null!;
 
         /// <summary>
         /// Collection of microphones
         /// </summary>
-        public DbSet<Microphone> Microphones { get; set; }
+        public DbSet<Microphone> Microphones { get; set; } = null!;
 
         /// <summary>
         /// Collection of brands
         /// </summary>
-        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Brand> Brands { get; set; } = null!;
 
         /// <summary>
         /// Collection of colors
         /// </summary>
-        public DbSet<Color> Colors { get; set; }
+        public DbSet<Color> Colors { get; set; } = null!;
 
         /// <summary>
         /// Collection of CPUs
         /// </summary>
-        public DbSet<CPU> CPUs { get; set; }
+        public DbSet<CPU> CPUs { get; set; } = null!;
 
         /// <summary>
         /// Collection of display coverages
         /// </summary>
-        public DbSet<DisplayCoverage> DisplayCoverages { get; set; }
+        public DbSet<DisplayCoverage> DisplayCoverages { get; set; } = null!;
 
         /// <summary>
         /// Collection of display sizes
         /// </summary>
-        public DbSet<DisplaySize> DisplaySizes { get; set; }
+        public DbSet<DisplaySize> DisplaySizes { get; set; } = null!;
 
         /// <summary>
         /// Collection of display technologies
         /// </summary>
-        public DbSet<DisplayTechnology> DisplayTechnologies { get; set; }
+        public DbSet<DisplayTechnology> DisplayTechnologies { get; set; } = null!;
 
         /// <summary>
         /// Collection of formats
         /// </summary>
-        public DbSet<Format> Formats { get; set; }
+        public DbSet<Format> Formats { get; set; } = null!;
 
         /// <summary>
         /// Collection of RAMs
         /// </summary>
-        public DbSet<RAM> RAMs { get; set; }
+        public DbSet<RAM> RAMs { get; set; } = null!;
 
         /// <summary>
         /// Collection of refresh rates
         /// </summary>
-        public DbSet<RefreshRate> RefreshRates { get; set; }
+        public DbSet<RefreshRate> RefreshRates { get; set; } = null!;
 
         /// <summary>
         /// Collection of resolutions
         /// </summary>
-        public DbSet<Resolution> Resolutions { get; set; }
+        public DbSet<Resolution> Resolutions { get; set; } = null!;
 
         /// <summary>
         /// Collection of sensitivities
         /// </summary>
-        public DbSet<Sensitivity> Sensitivities { get; set; }
+        public DbSet<Sensitivity> Sensitivities { get; set; } = null!;
 
         /// <summary>
         /// Collection of SSD capacities
         /// </summary>
-        public DbSet<SSDCapacity> SSDCapacities { get; set; }
+        public DbSet<SSDCapacity> SSDCapacities { get; set; } = null!;
 
         /// <summary>
         /// Collection of types
         /// </summary>
-        public DbSet<Type> Types { get; set; }
+        public DbSet<Type> Types { get; set; } = null!;
 
         /// <summary>
         /// Collection of video cards
         /// </summary>
-        public DbSet<VideoCard> VideoCards { get; set; }
+        public DbSet<VideoCard> VideoCards { get; set; } = null!;
     }
 }
--- PCShop/PCShop/StartUp.cs
+++ PCShop/PCShop/StartUp.cs
@@ -1,43 +1,38 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PCShop.Infrastructure.Data;
 using PCShop.Infrastructure.Data.Models.Account;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection"));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetValue<string>("ConnectionStrings:DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<User>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:RequireConfirmedAccount");
 
-    options.User.RequireUniqueEmail = true;
+    options.User.RequireUniqueEmail = builder.Configuration.GetValue<bool>("Identity:RequireUniqueEmail");
 
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromHours(24);
+    options.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:RequiredLength");
+    options.Password.RequireDigit = builder.Configuration.GetValue<bool>("Identity:RequireNonAlphanumeric");
+    options.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:RequireNonAlphanumeric");
+    options.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:RequireUppercase");
+    options.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:RequireLowercase");
 })
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
