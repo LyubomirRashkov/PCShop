4199a68f069ec85082cbb8fbe66e68aaaee1d5b0
Lyubomir <lyubomir.rashkov88@gmail.com>
2022-12-16 23:45:58 +0200

    Added integration test for ClientService-Guard

---
+++ PCShop/PCShop.Tests/IntegrationTests/ClientServiceGuardTests.cs
@@ -0,0 +1,36 @@
+﻿using PCShop.Core.Exceptions;
+using PCShop.Core.Services.Implementations;
+using PCShop.Core.Services.Interfaces;
+using PCShop.Infrastructure.Common;
+using PCShop.Tests.UnitTests;
+using static PCShop.Core.Constants.Constant.ClientConstants;
+
+namespace PCShop.Tests.IntegrationTests
+{
+	[TestFixture]
+	public class ClientServiceGuardTests : UnitTestsBase
+	{
+		private IRepository repository;
+		private IGuard guard;
+		private IClientService clientService;
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			this.repository = new Repository(this.data);
+			this.guard = new Guard();
+
+			this.clientService = new ClientService(this.repository, this.guard);
+		}
+
+		[Test]
+		public void GetNumberOfActiveSales_ShouldThrowPCShopExceptionWhenThereIsNoClientWithTheGivenUserIdInTheDb()
+		{
+			var userId = "invalid";
+
+			var ex = Assert.ThrowsAsync<PCShopException>(async () => await this.clientService.GetNumberOfActiveSales(userId));
+
+			Assert.That(ex.Message, Is.EqualTo(ErrorMessageForInvalidUserId));
+		}
+	}
+}
